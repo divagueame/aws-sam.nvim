56394490d9229b2a00018e5d6a8c57ad476e485a
divagueame <martin.arce.teixeira@gmail.com>
2024-09-29 20:05:42 +0200

    Keymaps

--- lua/aws-sam/keymaps.lua
+++ lua/aws-sam/keymaps.lua
@@ -3,7 +3,9 @@ local M = {}
 M.setup = function(opts)
   if opts.keymaps ~= false then
     vim.api.nvim_set_keymap("n", "<leader><leader>v", ":SamValidate<cr>", { desc = "Sam - Validate a template", noremap = true, silent = true })
-		-- vim.api.nvim_set_keymap("n", "<leader><leader>i", ":SamLocalInvoke<cr>", { desc = "Sam - Invoke function locally", noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader><leader>i", ":SamLocalInvoke<cr>", { desc = "Sam - Invoke function locally", noremap = true, silent = true })
+
+		vim.api.nvim_set_keymap("n", "<leader><leader>b", ":SamBuild<cr>", { desc = "Sam - Build", noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<leader><leader>f", ":SamLocalBuildInvokeFn<cr>", { desc = "Sam - Validate a template", noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<leader><leader>g", ":SamApiGatewayLocalStart<cr>", { desc = "Sam - Launch Api Gateway Locally", noremap = true, silent = true })
   end
